8da5fb6e54c268b33052d3c83bcea506b4699605
abdur-shobur <abdurshobur.developer@gmail.com>
2023-01-05 11:54:11 +0600

    learn object

--- output/arrayAndObject.js
+++ output/arrayAndObject.js
@@ -5,3 +5,9 @@ arr.push('sf');
 // string and number
 let arr2 = [];
 arr2.push('sfd', 324);
+// object
+let person;
+person = {
+    name: 'sdf',
+    age: 34,
+};
--- src/arrayAndObject.ts
+++ src/arrayAndObject.ts
@@ -5,3 +5,13 @@ arr.push('sf')
 // string and number
 let arr2: (string | number)[] = []
 arr2.push('sfd', 324)
+
+// object
+let person: {
+  name: string
+  age: number
+}
+person = {
+  name: 'sdf',
+  age: 34,
+}
