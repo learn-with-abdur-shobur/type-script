91e2bf62ce295f1b90055732ae715eaefd5b4581
abdur-shobur <abdurshobur.developer@gmail.com>
2023-09-07 22:45:40 +0600

    update ts:

--- output/types/build-in-types.js
+++ output/types/build-in-types.js
@@ -6,10 +6,46 @@ let objX = {
     name2: 'sh',
 };
 for (let x in objX) {
-    console.log(x);
+    // console.log(x);
 }
 // let en = array.entries();
 // for (let x of en) {
 // 	console.log(x);
 // }
 // console.log(en);
+let myTuple = ['sdf', 232];
+// console.log(myTuple);
+var Color;
+(function (Color) {
+    Color["Red"] = "Left";
+    Color["Green"] = "nbdfd";
+    Color["Blue"] = "Blue";
+})(Color || (Color = {}));
+let c;
+let ColorColor = Color.Green;
+// console.log(ColorColor);
+var Direction;
+(function (Direction) {
+    Direction["Up"] = "UP";
+    Direction["Down"] = "DOWN";
+    Direction["Left"] = "LEFT";
+    Direction["Right"] = "RIGHT";
+})(Direction || (Direction = {}));
+let dir = Direction.Left;
+let myDirection = Direction.Up;
+// console.log(dir); // Output: "UP"
+// class
+class PointX {
+    constructor(x) {
+        this.age = x;
+    }
+}
+class Point extends PointX {
+    constructor(x, y, age) {
+        super(age);
+        this.x = x;
+        this.y = y;
+    }
+}
+let em = new Point(3, 4, 324);
+console.log(em);
--- src/types/build-in-types.ts
+++ src/types/build-in-types.ts
@@ -6,7 +6,7 @@ let objX = {
 	name2: 'sh',
 };
 for (let x in objX) {
-	console.log(x);
+	// console.log(x);
 }
 
 // let en = array.entries();
@@ -15,3 +15,48 @@ for (let x in objX) {
 // 	console.log(x);
 // }
 // console.log(en);
+
+let myTuple: [string, number] = ['sdf', 232];
+
+// console.log(myTuple);
+
+enum Color {
+	Red = 'Left',
+	Green = 'nbdfd',
+	Blue = 'Blue',
+}
+let c: Color;
+let ColorColor = Color.Green;
+// console.log(ColorColor);
+
+enum Direction {
+	Up = 'UP',
+	Down = 'DOWN',
+	Left = 'LEFT',
+	Right = 'RIGHT',
+}
+
+let dir = Direction.Left;
+let myDirection: Direction = Direction.Up;
+// console.log(dir); // Output: "UP"
+
+// class
+
+class PointX {
+	age: number;
+	constructor(x: number) {
+		this.age = x;
+	}
+}
+class Point extends PointX {
+	x?: number;
+	y?: number;
+	constructor(x: number, y: number, age: number) {
+		super(age);
+		this.x = x;
+		this.y = y;
+	}
+}
+
+let em = new Point(3, 4, 324);
+console.log(em);
